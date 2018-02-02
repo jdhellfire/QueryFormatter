81f0f691c511b74b84ca4e2da01ab2ceeb33e8f5
jdhellfire <jdhellfire@126.com>
2018-02-02 23:21:47 +0800

    RED:lower trans ft created and run fail

--- FormatterFT.py
+++ FormatterFT.py
@@ -2,7 +2,16 @@ import unittest
 
 
 class QueryFormatterFT(unittest.TestCase):
-    pass
+
+    def test_lower_query_str(self):
+        """
+        GIVEN:query str with Upper chars
+        WHEN :call translate interface
+        THAN :translate all Upper chars to lower in query str
+        """
+        formatter = QueryFormatter()
+        ret = formatter.translate("HellO")
+        self.assertEqual("hello",ret)
 
 
 if __name__ == "__main__":
