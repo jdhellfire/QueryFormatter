b4452f4b180cca02c45a8d1327633e008d57a8b3
jdhellfire <jdhellfire@126.com>
2018-02-02 23:59:21 +0800

    RF: refactor for clean code

--- Formatter.py
+++ Formatter.py
@@ -1,6 +1,4 @@
 class QueryFormatter(object):
     def translate(self, quer_str):
-        if quer_str.startswith("\"") and quer_str.endswith("\""):
-            return quer_str.lower()
-        return quer_str.lower().replace('-', '\\-')
-
+        return quer_str.lower() if quer_str.startswith("\"") and quer_str.endswith("\"") \
+                                else quer_str.lower().replace('-', '\\-')
