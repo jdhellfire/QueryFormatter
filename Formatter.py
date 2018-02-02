c47044c820e68d423503ed7f63d5ffedbfef9d95
jdhellfire <jdhellfire@126.com>
2018-02-02 23:52:03 +0800

    GREEN:dish no replace scene pass

--- Formatter.py
+++ Formatter.py
@@ -1,3 +1,6 @@
 class QueryFormatter(object):
     def translate(self, quer_str):
+        if quer_str.startswith("\"") and quer_str.endswith("\""):
+            return quer_str.lower()
         return quer_str.lower().replace('-', '\\-')
+
