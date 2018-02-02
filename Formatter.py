746e37ef9c9ca10a7b7fa0c71252219ca5bdd97f
jdhellfire <jdhellfire@126.com>
2018-02-03 00:47:26 +0800

    GREEN:dish no replace scene case run pass

--- Formatter.py
+++ Formatter.py
@@ -1,4 +1,4 @@
 class QueryFormatter(object):
     def translate(self, quer_str):
         return quer_str.lower() if quer_str.startswith("\"") and quer_str.endswith("\"") \
-                                else quer_str.lower().replace('-', '\\-')
+            else quer_str.lower().replace('-', '\\-').replace("\\\\", "\\")
